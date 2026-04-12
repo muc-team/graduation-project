d3486b6f330e578012bea18a0fd55306660609ec
Mahmoud Matouk <110528830+Mahmoud-Matouk@users.noreply.github.com>
2026-04-12 22:11:51 +0200

    Remove NX client file, add robots, move Arduino code

--- classification/camera.py
+++ classification/camera.py
@@ -3,43 +3,10 @@ import time
 import pyttsx3 
 import threading
 from ultralytics import YOLO
-import speech_recognition as sr
 
-processing_enabled = False
+processing_enabled = True
 last_announced_object = None
 video_port = 0
-
-
-def voice_listener():
-    global processing_enabled
-    r = sr.Recognizer()
-
-    print("Voice listener started... Say 'go' or 'stop'")
-
-    while True:
-        try:
-            with sr.Microphone() as source:
-                r.adjust_for_ambient_noise(source, duration=0.5)
-                audio = r.listen(source)
-
-            command = r.recognize_google(audio, language="en-US").lower()
-            print(f"Command heard: {command}")
-
-            if "go" in command:
-                processing_enabled = True
-                print("--- Status: Classification enabled ---")
-            elif "stop" in command:
-                processing_enabled = False
-                print("--- Status: Classification paused ---")
-
-        except sr.UnknownValueError:
-            pass
-        except sr.RequestError as e:
-            print(f"Google Speech Recognition service error; {e}")
-        except Exception as e:
-            print(f"Error in voice listener thread: {e}")
-
-
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 
@@ -48,15 +15,10 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-model = YOLO('../models/yolov8n-seg.pt')
+model = YOLO('../models/yolov8n.pt')
 
 cap = cv2.VideoCapture(video_port)
 print("Camera is on... Press 'q' to quit")
-
-listener_thread = threading.Thread(target=voice_listener, daemon=True)
-listener_thread.start()
-
-
 while True:
     ret, frame = cap.read()
     if not ret:
